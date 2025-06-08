cc0f57b81e9721d2673bc2d3eace3fb960867792
TharlisSampaio <tharlis.sampaio@outlook.com>
2025-06-08 20:23:05 -0300

    adicionando o ChangeNotifierProvider disponivel para toda aplicação e adição de Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:desafio_agenda_front_end/src/pages/agenda_list_page.dart';
+import 'package:desafio_agenda_front_end/src/provider/agenda_provider.dart';
+import 'package:desafio_agenda_front_end/src/services/agenda_api.dart';
+import 'package:desafio_agenda_front_end/src/services/agenda_service.dart';
+import 'package:desafio_agenda_front_end/src/widgets/agenda_form.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AgendaProvider(AgendaService(AgendaApi())),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,9 +21,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ola meu chapa',
+    return MultiProvider(
+      providers: [
+        Provider<AgendaService>(create: (_) => AgendaService(AgendaApi())),
+      ],
+      child: Center(
+        child: MaterialApp(
+          title: 'Agenda',
+          theme: ThemeData(
+            primaryColor: Colors.lightBlue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          initialRoute: '/',
+          routes: {
+            '/': (context) => const AgendaListPage(),
+            '/agenda-form': (context) => const AgendaForm(),
+          },
+        ),
+      ),
     );
   }
 }
-
