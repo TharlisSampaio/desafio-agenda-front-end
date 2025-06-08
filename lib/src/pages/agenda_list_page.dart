38d8baa06b9b8c69d6c6938a3db8ff8a0c8b02cf
TharlisSampaio <tharlis.sampaio@outlook.com>
2025-06-08 20:19:29 -0300

    feat: page que lista todas as agendas

--- lib/src/pages/agenda_list_page.dart
+++ lib/src/pages/agenda_list_page.dart
@@ -1,4 +1,7 @@
+import 'package:desafio_agenda_front_end/src/models/agenda.dart';
 import 'package:desafio_agenda_front_end/src/provider/agenda_provider.dart';
+import 'package:desafio_agenda_front_end/src/services/agenda_api.dart';
+import 'package:desafio_agenda_front_end/src/services/agenda_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -20,6 +23,7 @@ class _AgendaListPageState extends State<AgendaListPage> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Agenda'),
@@ -29,71 +33,76 @@ class _AgendaListPageState extends State<AgendaListPage> {
             icon: Icon(Icons.add_sharp),
             color: Colors.black54,
             onPressed: () {
-              print('botão adicionar new agenda');
+              Navigator.pushNamed(context, '/agenda-form');
             },
           ),
         ],
       ),
-      body: Consumer<AgendaProvider>(
-        builder: (context, agendaProvider, child) {
-          if (agendaProvider.isLoading) {
-            return Center(child: CircularProgressIndicator());
-          }
-          if (agendaProvider.errorMessage != null) {
-            return Center(
-              child: Text(
-                agendaProvider.errorMessage!,
-                style: TextStyle(color: Colors.red),
-              ),
-            );
-          }
-          if (agendaProvider.agenda.isEmpty) {
-            return Center(child: Text('Nenhuma tarefa encontrada.'));
-          }
-          return ListView.builder(
-            itemCount: agendaProvider.agenda.length,
-            itemBuilder: (context, index) {
-              final agenda = agendaProvider.agenda[index];
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Consumer<AgendaProvider>(
+          builder: (context, agendaProvider, child) {
+            if (agendaProvider.isLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (agendaProvider.errorMessage != null) {
               return Center(
-                child: Card(
-                  color: const Color.fromARGB(255, 238, 234, 234),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ListTile(
-                        title: Text('${agenda.nome}'),
-                        subtitle: Text('${agenda.telefone}'),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          IconButton(
-                            icon: Icon(
-                              Icons.edit_sharp,
-                              color: Colors.green[300],
-                            ),
-                            onPressed: () {
-                              print('Botão de editar');
-                            },
-                          ),
-                          IconButton(
-                            icon: Icon(
-                              Icons.delete_sharp,
-                              color: Colors.redAccent,
-                            ),
-                            onPressed: () {
-                              print('Botão de deletar');
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                child: Text(
+                  agendaProvider.errorMessage!,
+                  style: TextStyle(color: Colors.red),
                 ),
               );
-            },
-          );
-        },
+            }
+            if (agendaProvider.agenda.isEmpty) {
+              return Center(child: Text('Nenhuma tarefa encontrada.'));
+            }
+            return ListView.builder(
+              itemCount: agendaProvider.agenda.length,
+              itemBuilder: (context, index) {
+                final agenda = agendaProvider.agenda[index];
+                return Center(
+                  child: Card(
+                    color: const Color.fromARGB(255, 238, 234, 234),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListTile(
+                          title: Text('${agenda.nome} ${agenda.sobrenome}'),
+                          subtitle: Text('${agenda.telefone}'),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                              icon: Icon(
+                                Icons.edit_sharp,
+                                color: Colors.green[300],
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).pushNamed('/agenda-form', arguments: agenda);
+                                print('Botão de editar');
+                              },
+                            ),
+                            IconButton(
+                              icon: Icon(
+                                Icons.delete_sharp,
+                                color: Colors.redAccent,
+                              ),
+                              onPressed: () {
+                                agendaProvider.deleteAgenda(agenda.id.toString());
+                                print('Botão de deletar');
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
